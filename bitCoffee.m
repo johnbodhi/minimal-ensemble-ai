5e522ee45a0fa0519fe9f477b3a2165003a8215f
johnbodhi <61892807+johnbodhi@users.noreply.github.com>
2025-04-18 15:22:38 -0700

    Update

--- bitCoffee.m
+++ bitCoffee.m
@@ -27,7 +27,7 @@ function  [ B_ ] = bitCoffee( J_, B_, SP_ )
     
         SP_( B_(J_), J_+1 ) = SP_( B_(J_), J_+1 ) + 1;   
         
-        B_(J_)              = SP_(B_(J_),J_);
+        B_( J_)             = SP_(B_(J_),J_);
         
         SP_( B_(J_), J_ )   = SP_( B_(J_), J_   ) - 1;                                                                                                                                                                                                     
     
