b64a110e2be0242db6c13221be6c32f10ca126ce
johnbodhi <61892807+johnbodhi@users.noreply.github.com>
2025-05-04 14:03:52 -0700

    Update

--- bitCoffee.m
+++ bitCoffee.m
@@ -50,31 +50,31 @@ function  [ B_, SP_ ] = bitCoffee( J_, B_, SP_ )
         
         SP_( B_(J_), J_ )       = SP_( B_(J_), J_ ) + 1;
     
-    elseif ( B_(J_-1) && SP_(B_(J_-1),J_-1) && B_(J_) && SP_(B_(J_),J_) )
+    elseif ( B_(J_-1) && SP_(B_(J_-1),J_-1) && B_(J_) && SP_(B_(J_),J_)   )
         
 
-    elseif ( B_(J_-1) && SP_(B_(J_-1),J_-1) && B_(J_) && SP_(B_(J_),J_) )
+    elseif ( B_(J_-1) && SP_(B_(J_-1),J_-1) && B_(J_) && SP_(B_(J_),J_)   )
 
 
-    elseif ( B_(J_-1) && SP_(B_(J_-1),J_-1) && B_(J_) && SP_(B_(J_),J_) )
+    elseif ( B_(J_-1) && SP_(B_(J_-1),J_-1) && B_(J_) && SP_(B_(J_),J_)   )
 
 
-    elseif ( B_(J_-1) && SP_(B_(J_-1),J_-1) && B_(J_) && SP_(B_(J_),J_) )
+    elseif ( B_(J_-1) && SP_(B_(J_-1),J_-1) && B_(J_) && SP_(B_(J_),J_)   )
 
 
-    elseif ( B_(J_-1) && SP_(B_(J_-1),J_-1) && B_(J_) && SP_(B_(J_),J_) )
+    elseif ( B_(J_-1) && SP_(B_(J_-1),J_-1) && B_(J_) && SP_(B_(J_),J_)   )
 
 
-    elseif ( B_(J_-1) && SP_(B_(J_-1),J_-1) && B_(J_) && SP_(B_(J_),J_) )
+    elseif ( B_(J_-1) && SP_(B_(J_-1),J_-1) && B_(J_) && SP_(B_(J_),J_)   )
 
 
-    elseif ( B_(J_-1) && SP_(B_(J_-1),J_-1) && B_(J_) && SP_(B_(J_),J_) )
+    elseif ( B_(J_-1) && SP_(B_(J_-1),J_-1) && B_(J_) && SP_(B_(J_),J_)   )
 
 
-    elseif ( B_(J_-1) && SP_(B_(J_-1),J_-1) && B_(J_) && SP_(B_(J_),J_) )
+    elseif ( B_(J_-1) && SP_(B_(J_-1),J_-1) && B_(J_) && SP_(B_(J_),J_)   )
 
 
-    elseif ( B_(J_-1) && SP_(B_(J_-1),J_-1) && B_(J_) && SP_(B_(J_),J_) )
+    elseif ( B_(J_-1) && SP_(B_(J_-1),J_-1) && B_(J_) && SP_(B_(J_),J_)   )
 
 
     end
