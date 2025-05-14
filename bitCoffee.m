45018139f5c1db3e29166c40b0db9754848dda50
johnbodhi <61892807+johnbodhi@users.noreply.github.com>
2025-05-14 13:59:02 -0700

    Update

--- bitCoffee.m
+++ bitCoffee.m
@@ -18,15 +18,15 @@ function  [ B_, SP_ ] = bitCoffee( J_, B_, SP_ )
     
         B_(J_)                = B_(J_-1);  
         
-        SP_( B_(J_-1), J_-1 ) = SP_( B_(J_-1), J_-1 ) - 1;                                                                                                                                                                                             
+        SP_( B_(J_-1), J_-1 ) = SP_( B_(J_-1), J_-1 ) - 1;                                                                                                                                                                                            
         
         B_(J_-1)              = SP_( B_(J_), J_ );                                                                                                                                                                                                                                                                                                                                                                         
                                                                                                                                  
     elseif ( B_(J_-1) && ~SP_(B_(J_-1),J_-1) && B_(J_) && ~SP_(B_(J_),J_) )
     
-        SP_( B_(J_-1), J_-1 ) = SP_( B_(J_), J_-1 ) + 1;                                                                                                                                                                                     
+        SP_( B_(J_), J_-1 ) = SP_( B_(J_), J_-1 ) + 1;                                                                                                                                                                                     
         
-        B_(J_)                = 0;                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
+        B_(J_)              = 0;                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                  
     
     elseif ( B_(J_-1) && SP_(B_(J_-1),J_-1) && B_(J_) && ~SP_(B_(J_),J_)  )
     
