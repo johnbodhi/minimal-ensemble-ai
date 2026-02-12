420a518c466a4b95cda853b7aaf76f75420233e3
johnbodhi <61892807+johnbodhi@users.noreply.github.com>
2026-02-12 14:16:54 -0700

    Add files via upload

---
+++ minmal-ensemble-ai/MATLAB/Kernel Methods/bitFlip.m
@@ -0,0 +1,79 @@
+function  [B_,SP_] = bitFlip(J_,B_,STL_,STU_,SP_,SPNC_,SPC_)
+
+        M_        = size(B_,2)                                                                   ;
+
+        % [MX_,IX_] = max(SP_,[],'linear')                                                       ;
+
+        K_        = size(find(SPNC_(B_(J_),J_)),1)                                               ;
+    
+    if     ( B_(J_-1) && ~SPNC_(B_(J_-1),J_-1) && ~B_(J_) && ~SPNC_(B_(J_),J_)                   )                                                                                                                                                                      
+    
+        B_(J_)                    = B_(J_-1)                                                     ;
+         
+        B_(J_-1)                  = 0                                                            ;
+
+        if ( J_ == M_  )                                                                         )
+ 
+            if ( SPNC_(B_(STL_(I_)),J_)                                                          )
+
+                SPNC_(B_(J_), STL_(I_)) = SPNC_(B_(J_),STL_(I_)) + 1                             ; 
+
+                SPC_              = SPC_ + 1                                                     ;
+            end
+            
+            if (                                                                                 )                                                                 
+
+                B_(STL_(J_))       = B_(J_)                                                      ;
+                B_(J_)             = 0                                                           ;
+            end
+            
+        end
+    
+    elseif ( B_(J_-1) && SPNC_(B_(J_-1), J_-1) && ~B_(J_) && ~SPNC_(B_(J_), J_)                  )                                                                                                                                                                                                          
+    
+        B_(J_)                    = B_(J_-1)                                                     ;
+
+        B_(J_-1)                  = SPNC_(B_(J_),J_-1)                                           ;
+        
+        SPNC_(B_(J_-1), J_-1)     = SPNC_(B_(J_-1), J_-1) - 1                                    ;   
+
+        SPC_                      = SPC_ - 1                                                     ;   
+                                                                                                                                 
+    elseif ( B_(J_-1) && ~SPNC_(B_(J_-1),J_-1) && B_(J_) && ~SPNC_(B_(J_),J_)                    )
+    
+        SPNC_(B_(J_), J_)         = SPNC_(B_(J_), J_) + 1                                        ;    
+
+        SP_(B_(J_),J_,K_+1)       = SPC_                                                         ;
+        
+        B_(J_-1)                  = 0                                                            ;   
+
+        SPC_                      = SPC_ + 1                                                     ;   
+    
+    elseif ( B_(J_-1) && SPNC_(B_(J_-1),J_-1) && B_(J_) && ~SPNC_(B_(J_),J_)                     )
+
+        B_(J_-1)                  = SPNC_(B_(J_-1),J_-1)                                         ;
+
+        SPNC_(B_(J_-1), J_-1)     = SPNC_(B_(J_-1), J_-1) - 1                                    ;  
+    
+        SPNC_( B_(J_), J_ )       = SPNC_( B_(J_), J_ ) + 1                                      ;         
+    
+    elseif ( B_(J_-1) && ~SPNC_(B_(J_-1),J_-1) && B_(J_) && SPNC_(B_(J_),J_)                     )
+    
+        SPNC_(B_(J_),J_)          = SPNC_(B_(J_),J_) + 1                                         ;
+        
+        B_(J_-1)                  = 0                                                            ;
+
+        SPC_                      = SPC_ + 1                                                     ;   
+    
+    elseif ( B_(J_-1) && SPNC_(B_(J_-1),J_-1) && B_(J_) && SPNC_(B_(J_),J_)                      )
+    
+        B_(J_-1)                  = SPNC_(B_(J_-1), J_-1)                                        ;
+        
+        SPNC_(B_(J_-1), J_-1)     = SPNC_(B_(J_-1),J_-1) - 1                                     ;
+
+    elseif ( ~B_(J_-1) && SPNC_(B_(J_-1), J_-1) && ~B_(J_) && ~SPNC_(B_(J_), J_)                 )                                                                                                                                                                                                
+
+        B_(J_-1)                  = B_(J_-1)                                                     ;
+
+        SPNC_(B_(J_), J_-1)       = SPNC_(B_(J_), J_-1) - 1                                      ;         
+    end
